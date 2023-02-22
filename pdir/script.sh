cf93ee553272e11d8771b1f781f73794ebf452bd
Nicholas Sidharta <toxicancm@gmail.com>
2023-02-22 11:18:37 +0700

    Nicholas/ Input Validation Script finish

--- pdir/script.sh
+++ pdir/script.sh
@@ -26,24 +26,34 @@ function get_values(){
     do
         if [[ $line == *"INPUT="* ]]; then
             echo "script.sh: Getting Input Path..."
+            # Take the value
             input=${line#*=}
+            # Clean the value from \r, \n, and extra space
+            input=$(echo $input | sed 's/[[:space:]\r\n]//g')
             echo "script.sh: Input Path: $working_dir/$input"
         fi
 
         if [[ $line == *"OUTPUT="* ]]; then
             echo "script.sh: Getting Output Path..."
+            # Take the value
             output=${line#*=}
+            # Clean the value from \r, \n, and extra space
+            output=$(echo $output | sed 's/[[:space:]\r\n]//g')
             echo "script.sh: Output Path: $working_dir/$output"
         fi
 
         if [[ $line == *"CURRENT_WEEK="* ]]; then
             echo "script.sh: Getting Current Week..."
+            # Take the value
             week=${line#*=}
+            # Clean the value from \r, \n, and extra space
+            week=$(echo $week | sed 's/[[:space:]\r\n]//g')
             echo "script.sh: Week: $week"
         fi
 
     done < "$config"
 
+    # Checking if any configuration was not set
     if [[ -z $input ]]; then
         echo "script.sh: Input directory is not set"
         exit 1
